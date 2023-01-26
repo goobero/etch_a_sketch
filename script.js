aa3005296abfcab3be9883fcc53cc23b67944db8
cherry <cherryishairy@gmail.com>
2023-01-26 21:54:14 +1100

    write and fix makeGrid function

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-const gridContainer =  document.getElementsByClassName('grid-container');
+const defaultSize = 16;
+const gridContainer =  document.getElementById('grid-container');
 
-for (i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.className = 'grid-square';
-    gridContainer[0].appendChild(div);
-}
+function makeGrid(size) {
+    gridContainer.style.gridTemplateColumns =`repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-const gridSquare = document.getElementsByClassName('grid-square');
-const gridSquareArr = Array.from(gridSquare);
+    for (i = 0; i < size * size; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        gridSquare.addEventListener('mouseover', (event) => {
+            event.target.style.backgroundColor = 'pink';
+        });
+        gridContainer.appendChild(gridSquare);
+    }
+};
 
-gridSquareArr.forEach((gridSquareArr) => {
-    gridSquareArr.addEventListener('mouseover', (event) => {
-        event.target.style.backgroundColor = 'blue';
-    });
-});
 
-const button = document.createElement('button');
-button.textContent = "hello";
-document.body.appendChild('button');
+/*function newSize(size) {
+    let num = prompt("Please enter the number of squares per side you want for the grid.")
+    let newSize = parseInt(num);
+    if (newSize < 100) {
+        setGrid(size);
+    } else {
+        alert("Sorry, please choose a number smaller than 100");
+    }
+};
+*/
+
+makeGrid(defaultSize);
