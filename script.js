20d815261549dae1488abbd5a0724328352badd5
cherry <cherryishairy@gmail.com>
2023-01-27 22:08:11 +1100

    add colour picker button

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const defaultSize = 16;
 const gridContainer =  document.getElementById('grid-container');
 const rainbowButton = document.getElementById('rainbow-button');
+const colourPicker = document.getElementById('colourpicker');
 
-let nowSize = defaultSize;
 let rainbowMode = false;
+let colourPickerMode = false;
 
 rainbowButton.onclick = () => rainbowMode = true;
+colourPicker.onclick = () => {
+    rainbowMode ===false;
+    colourPickerMode = true;
+};
 
 function makeGrid(size) {
     gridContainer.style.gridTemplateColumns =`repeat(${size}, 1fr)`;
@@ -22,10 +27,17 @@ function changeColour(event) {
     if (rainbowMode === true) {
         getRandomColour(event);
     }
-    else {
-        event.target.style.backgroundColor = 'pink';
+    else if (colourPickerMode === true) {
+        getPickedColour(event);
+    } else {
+        event.target.style.backgroundColor = 'black';
     }
 };
+
+function getPickedColour(event) {
+    let pickedColour = document.getElementById('colourpicker').value;
+    event.target.style.backgroundColor = pickedColour;
+}
 
 function resizeGrid(newSize) {
     let num = prompt("Please enter a number specifying the number of rows/columns you want the grid to have.");
@@ -49,7 +61,6 @@ function resizeGrid(newSize) {
 function resetGrid() {
     gridContainer.innerHTML=''
     makeGrid(defaultSize);
-    rainbowMode = false;
 }
 
 function getRandomColour(event) {
